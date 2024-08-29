dd63234f4936d34caa2be53dd569d9f60e9d180f
twistedboxes <tamponiandrea@gmail.com>
2024-08-29 10:05:43 +0200

    add two projects, implemented mapping through projects, edit styling of ProjectCard

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -9,23 +9,21 @@ interface IProjectCard {
 const ProjectCard = ({ title, stack, link, desc, image }: IProjectCard) => {
   return (
     <div className="flex h-screen w-full shrink-0 snap-center flex-col items-center justify-center gap-0 bg-[--color-primary] p-7 text-xl">
-      <div className="flex h-3/4 flex-col items-center justify-center rounded-sm bg-white p-4 py-8 shadow-2xl lg:w-3/4">
+      <div className="flex h-3/4 flex-col items-center justify-center rounded-sm px-6 bg-white p-4 py-8 shadow-2xl lg:w-3/4">
         <div
           style={{
             backgroundImage: `url(${image})`,
-            backgroundPosition: "center",
+            backgroundPosition: "top",
             backgroundSize: "cover",
             backgroundRepeat: "no-repeat",
+            border: "0.2em solid ",
+            marginBottom: "1em",
           }}
-          className="mb-2 h-full w-full p-2"
+          className="mb-2 h-full w-full"
         ></div>
         <div className="w-full px-2 text-center lg:flex lg:justify-between lg:text-left">
           <h2 className="text-2xl font-black decoration-[--color-accent] hover:text-[--color-primary] hover:underline lg:w-1/3">
-            <a
-              href={link}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <a href={link} target="_blank" rel="noopener noreferrer">
               {title}
             </a>
           </h2>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,39 +2,58 @@ import { Link } from "react-router-dom";
 import AnimatedScroll from "./AnimatedScroll";
 import ProjectCard from "./ProjectCard";
 import letitallpreview from "../assets/letitallpreview.png";
+import easyCryptoPreview from "../assets/easyCryptoPreview.png";
+import photographerPortfolio from "../assets/photographerPortfolio.png";
+
+const listOfProjects = [
+  {
+    title: "Let It All Out",
+    desc: "Simple journaling tool with a stress-levels tracker",
+    stack: ["NextJS", "Tailwind", "MongoDB", "Vercel"],
+    link: "https://www.letitallout.xyz",
+    image: letitallpreview,
+  },
+  {
+    title: "Easy Crypto",
+    desc: "A way to check the current status of the top 100 Cryptocurrencies in the world, using API calls",
+    stack: ["React", "Tailwind", "Netlify"],
+    link: "https://easycryptoviewer.netlify.app/",
+    image: easyCryptoPreview,
+  },
+  {
+    title: "Photographer Portfolio Concept",
+    desc: "A Showcase website",
+    stack: ["React", "Tailwind", "Netlify"],
+    link: "https://jenna-doe-photography.netlify.app/",
+    image: photographerPortfolio,
+  },
+];
+
+const projectPreview = listOfProjects.map((project) => {
+  return (
+    <ProjectCard
+      link={project.link}
+      image={project.image}
+      title={project.title}
+      desc={project.desc}
+      key={project.title}
+      stack={project.stack}
+    />
+  );
+});
 
 const Projects = () => {
   return (
     <>
       <AnimatedScroll />
       <div className="flex h-[100vh] w-full snap-y snap-mandatory flex-col justify-center overflow-y-scroll scroll-smooth bg-[--color-secondary] leading-3">
-        <h1 className="lg:w-2/3 pb-10 lg:text-left text-center select-none snap-center break-words text-6xl font-black uppercase lg:leading-[0.8em] lg:ml-12 mt-[320vh] lg:mt-[190%] lg:text-[12em]">
+        <h1 className="mt-[320vh] select-none snap-center break-words pb-10 text-center text-6xl font-black uppercase lg:ml-12 lg:mt-[190%] lg:w-2/3 lg:text-left lg:text-[12em] lg:leading-[0.8em]">
           Recent projects
         </h1>
         <h2 className="mb-40 mt-[-1.8rem] w-full break-words pb-8 text-center text-xl font-black uppercase tracking-widest text-[--color-primary] lg:ml-12 lg:text-left">
           Have a look at some of the projects I’ve worked on.
         </h2>
-        <ProjectCard
-          title="Let It All Out"
-          desc="Simple journaling tool with a stress-levels tracker"
-          stack={["NextJS", "Tailwind", "MongoDB"]}
-          link="https://www.letitallout.xyz"
-          image={letitallpreview}
-        />
-        <ProjectCard
-          title="Let It All Out"
-          desc="Simple journaling tool with a stress-levels tracker"
-          stack={["NextJS", "Tailwind", "MongoDB"]}
-          link="https://www.letitallout.xyz"
-          image={letitallpreview}
-        />
-        <ProjectCard
-          title="Let It All Out"
-          desc="Simple journaling tool with a stress-levels tracker"
-          stack={["NextJS", "Tailwind", "MongoDB"]}
-          link="https://www.letitallout.xyz"
-          image={letitallpreview}
-        />
+        {projectPreview}
       </div>
       <Link
         className="fixed right-0 top-0 mt-8 bg-black text-2xl font-black leading-[0.3em] text-white hover:bg-[--color-primary]"
