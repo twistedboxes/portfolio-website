c632840cb5329782ce561c8db5147a1f024e2cfe
twistedboxes <tamponiandrea@gmail.com>
2024-10-08 17:30:29 +0200

    cleanup unused imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { HashRouter as Router, BrowserRouter, Route, Link, Routes } from "react-router-dom";
+import {  BrowserRouter } from "react-router-dom";
 import AnimatedRoutes from "./components/AnimatedRoutes.js";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
